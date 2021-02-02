2fef41f13080a3880c8adaafe47f0e849d47ad4a
pasqua1e <31853954+pasqua1e@users.noreply.github.com>
2021-02-02 19:49:27 +0000

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,21 +1,9 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_ami" "ec2_instance" {
-  most_recent = true
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["${data.aws_caller_identity.current.account_id}"]
-}
-
-resource "aws_instance" "web" {
-  ami           = "${data.aws_ami.ec2_instance.id}"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "EC2 Demo Instance"
-  }
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 15
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+  max_password_age               = 79
 }
